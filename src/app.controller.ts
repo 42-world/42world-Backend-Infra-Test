f38cd3d7f09a58750c3b71d7cc874caad04d96cc
huni <petercha2000@gmail.com>
2023-03-26 23:26:37 +0900

    feat: aws ssecret manager

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('secret')
+  async getSecret(): Promise<string> {
+    return await this.appService.getSecret();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getSecret(): Promise<string> {
+    const secret_name = 'infra-test-env';
+
+    const client = new SecretsManagerClient({
+      credentials: {
+        accessKeyId: this.configService.getOrThrow('AWS_ACCESS_KEY'),
+        secretAccessKey: this.configService.getOrThrow('AWS_SECRET_KEY'),
+      },
+      region: 'ap-northeast-2',
+    });
+
+    const response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: 'AWSCURRENT', // VersionStage defaults to AWSCURRENT if unspecified
+      }),
+    );
+
+    const secret = response.SecretString;
+
+    return secret;
   }
 }
